2daa196c372c1115fd14f5ceac76d49d8da80b0f
unknown <mohdniaal@gmail.com>
2021-08-28 20:17:00 -0700

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TrialsComponent } from './trials/trials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TrialsComponent
   ],
   imports: [
     BrowserModule,
